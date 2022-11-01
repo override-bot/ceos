962f695766bdeadea61c2168d9d91d91c6d85155
Chukwudi Wisdom <55758868+override-bot@users.noreply.github.com>
2022-11-01 01:39:57 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ceos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ceos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color ceoPink = Color(0xfff5537b);
+const Color ceoPurple = Color.fromARGB(255, 26, 3, 39);
+const Color ceoPurpleGrey = Color.fromARGB(255, 135, 124, 141);
+const Color ceoWhite = Colors.white;
+const Color ceoBlack = Colors.black;
+const Color grey = Colors.grey;
